5aa171e4afc8ccf88cd75da4201ef157bdb08646
Dr.hossein Shabani <shabanimehran@gmail.com>
2025-09-11 01:55:20 +0330

    Delete django_server directory

--- django_server/coredb/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoredbConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coredb'
-    verbose_name = 'Core Database'
